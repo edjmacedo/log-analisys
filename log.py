d7f60c67d9a2dce168c933dfd78b11f601f9af47
Edvan Macedo <edvan.macedo.jr@gmail.com>
2017-06-20 22:19:39 -0400

    Implement the selection of who are the most popular article authors

--- log.py
+++ log.py
@@ -25,6 +25,26 @@ def get_most_popular_articles():
                         """)
     return db_cursor.fetchall()
 
+def get_most_popular_author():
+    db_cursor.execute("""
+                        SELECT
+                            authors.name,
+                            count(log.path) as views
+                        FROM
+                            articles,
+                            authors,
+                            log
+                        WHERE
+                            log.path=CONCAT('/article/', articles.slug)
+                        AND
+                            articles.author=authors.id
+                        GROUP BY
+                            authors.id
+                        ORDER BY
+                            views
+                        DESC
+                        """)
+    return db_cursor.fetchall()
 
 DBNAME = "news"
 db_connection = psycopg2.connect(database=DBNAME)
@@ -33,6 +53,11 @@ db_cursor = db_connection.cursor()
 print("What are the most popular three articles of all time?")
 most_popular_articles = get_most_popular_articles()
 for i in most_popular_articles:
+    print('"{}" — {} views'.format(i[0], i[1]))
+
+print("Who are the most popular article authors of all time?")
+most_popular_author = get_most_popular_author()
+for i in most_popular_author:
     print('"{}" — {} views'.format(i[0], i[1]))
 
 db_connection.close()
